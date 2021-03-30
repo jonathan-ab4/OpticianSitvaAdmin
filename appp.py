2a765027f332c30099653861ee9e87c5643b363a
jonathan-ab4 <59428485+jonathan-ab4@users.noreply.github.com>
2021-03-30 15:26:02 +0530

    Add files via upload

--- appp.py
+++ appp.py
@@ -42,67 +42,6 @@ default_app = initialize_app(cred)
 db = firestore.client()
 todo_ref = db.collection('doctor')
 
-
-@app.route('/update/doctor_id=<id>/<u>', methods=['GET'])
-def update(id,u):
-   
-    try:
-        
-        if(u == 'a'):
-
-            todo_ref.document(id).update({
-            'status': '1'})
-
-        else:
-
-            todo_ref.document(id).update({
-            'status': '0'})
-
-        return redirect(url_for('doctor'))
-
-    except Exception as e:
-        return f"An Error Occured: {e}"
-
-
-@app.route('/delete/doctor_id=<id>', methods=['GET'])
-def delete(id):
-   
-    try:
-        
-        todo_ref.document(id).delete()
-
-        return redirect(url_for('doctor'))
-
-    except Exception as e:
-        return f"An Error Occured: {e}"
-
-@app.route('/reset', methods=['GET', 'POST'])
-def reset():
-
-    uns = "Please enter details"
-
-    uns2 = "Password reset link sent"
-
-    if request.method == 'POST':
-
-        emailreset = request.form['email']
-
-        try:
-
-
-            auth.send_password_reset_email(emailreset)
-
-            return render_template('loginpage.html',s=uns2)
-
-            
-        except:
-
-            return render_template('reset.html', us=uns)
-
-    return render_template('reset.html')
-
-    
-
 @app.route('/', methods=['GET', 'POST'])
 def login():
 
@@ -130,6 +69,101 @@ def login():
             return render_template('loginpage.html', us=uns)
 
     return render_template('loginpage.html')
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+
+    global is_logged_in
+
+    session.permanent = True
+
+    is_logged_in = False
+
+    uns = "Please verify your mail"
+    inv = "Invalid email or incorrect password"
+
+    if request.method == 'POST':
+        email = request.form['name']
+        password = request.form['pass']
+
+        try:
+
+            user = auth.sign_in_with_email_and_password(email, password)
+            acc = auth.get_account_info(user["idToken"])
+
+            temp = json.dumps(acc)
+    
+            acc_info = json.loads(temp)
+
+            user_info = acc_info["users"]
+
+            emailVerified = str([sub['emailVerified'] for sub in user_info])
+
+
+            if(emailVerified.lower() != "[false]"):
+                
+                is_logged_in = True
+
+                doc_ref = db.collection('admin').document(user["localId"])
+                doc = doc_ref.get()
+                if doc.exists:
+                    db.collection("admin").document(user["localId"]).update({u'verified': True})
+                
+                else:
+                    data = { 'email': email , 'verified': True}
+                    db.collection("admin").document(user["localId"]).set(data)
+            
+                return redirect(url_for('user'))
+	            
+    
+            else:
+                return  render_template('signinpage.html', us=uns)
+
+                
+        except:
+             return render_template('signinpage.html',msg=inv)
+    else:
+        if is_logged_in == True:
+            return redirect(url_for('user'))
+        else:
+            return render_template('signinpage.html')
+
+
+
+@app.route('/reset', methods=['GET', 'POST'])
+def reset():
+
+    uns = "Please enter details"
+
+    uns2 = "Password reset link sent"
+
+    if request.method == 'POST':
+
+        emailreset = request.form['email']
+
+        try:
+
+
+            auth.send_password_reset_email(emailreset)
+
+            return render_template('loginpage.html',s=uns2)
+
+            
+        except:
+
+            return render_template('reset.html', us=uns)
+
+    return render_template('reset.html')
+
+
+
+@app.route('/user', methods=['GET'])
+def user():
+    if is_logged_in == True:
+        return render_template('user.html')
+    else:
+        return redirect(url_for('signin'))
 
 @app.route('/userDetails', methods=['POST'])
 def appointment():
@@ -181,14 +215,13 @@ def appointment():
 
             else:
                 return render_template("userDetails.html", us=res2, uid=doc_id,user = userList)
-    # else:
-    #     return redirect(url_for('user'))
+   
 
 
 @app.route('/editUser/uid=<uid>', methods=['GET', 'POST'])
 def editUser(uid):
     try:
-        # if request.method == 'POST':
+        
         addr = request.form['addr']
         name = request.form['uname']
 
@@ -196,12 +229,47 @@ def editUser(uid):
         
         db.collection(u'user').document(uid).update({'name': name, 'address_google_map': addr})
 
-        # return render_template("userDetails.html", us=res2, uid=doc_id, user=userList)
         return render_template('user.html')
 
     except Exception as e:
         return f"An Error Occurred: {e}"
+
+
+@app.route('/update/doctor_id=<id>/<u>', methods=['GET'])
+def update(id,u):
+   
+    try:
         
+        if(u == 'a'):
+
+            todo_ref.document(id).update({
+            'status': '1'})
+
+        else:
+
+            todo_ref.document(id).update({
+            'status': '0'})
+
+        return redirect(url_for('doctor'))
+
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+@app.route('/delete/doctor_id=<id>', methods=['GET'])
+def delete(id):
+   
+    try:
+        
+        todo_ref.document(id).delete()
+
+        return redirect(url_for('doctor'))
+
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+          
 
 @app.route('/customer', methods=['GET'])
 def customer():
@@ -217,146 +285,15 @@ def doctor():
     
 
 
-@app.route('/user', methods=['GET'])
-def user():
-    if is_logged_in == True:
-        return render_template('user.html')
-    else:
-        return redirect(url_for('signin'))
 
-
-
-# @app.route('/check',methods =['GET','POST'])
-# def check():
-
-#     doc={}
-
-#     doc_t=[]
-
-#     res2=[]
-
-    
-#     docs = db.collection(u'user').where(u'email', u'==', test_email).stream()
-
-
-#     for doc in docs:
-#         doc1 = doc.to_dict()
-#         doc_id = doc.id
-
-#     appoint = db.collection(u'appointment').where(u'user_id', u'==', doc_id).stream()
-
-    
-
-#     if(not doc):
-               
-#         return "nothing"
-
-#     else:
-
-#         for doc in appoint:
-#             # print(f'{doc.id} => {doc.to_dict()}')
-#             appointment = doc.to_dict()
-#             doc_t.append(appointment)
-
-#         res = [ sub['epoch'] for sub in doc_t ] 
-
-#         for res1 in res:
-#             stored_time = time.strftime("%d-%m-%Y %H:%M:%S",time.localtime(res1/1000))
-#             res2.append(stored_time)
-
-#         print(res2)
-
-#         if(not doc_t):
-#             return "Nothing2.0"
-
-#         else:
-
-#             return render_template("base.html",us=res2,uid=doc_id)
-
-# def ap():
-#     return redirect(url_for('user'))
-    # if request.method == 'POST':
-    #     uid = request.form['uid']
-    #     epoch = request.form['epoch']
-    #     print(request.json)
-    #     get_epoch = time.mktime(time.strptime(epoch, '%d-%m-%Y %H:%M:%S')) * 1000
-
-    #     appoint = db.collection(u'appointment').where(u'user_id', u'==', uid).stream()
-    #     doc_t = []
-
-    #     print(get_epoch)
-
-
-    #     for doc in appoint:
-    #         # print(f'{doc.id} => {doc.to_dict()}')
-    #         appointment = doc.to_dict()
-    #         doc_t.append(appointment)
-
-    #     for e in doc_t:
-
-    #         if(e['epoch']== get_epoch):
-
-    #             epoch_app = e
-    #             print(epoch_app)
-    #             return "hello"
-    #             # return "render_template("base.html",us=epoch_app)"
-    #             # return redirect(url_for('view_app'))
-
-                
-    #         # //return "redirect(url_for('view_app'))"
-        
-    #     return "hi"
-
-
-            #return render_template("base.html",us=epoch,uid=uid)
-
-
-   
 
 
 @app.route('/logout')
 def logout():
     is_logged_in = False
-    # print(is_logged_in)
     return redirect(url_for('signin'))
 
 
-
-
-   
-
-# @app.route('/check/appointment/user=<uid>/app_id=<epoch>',methods =['GET','POST'])
-# def checkepoch(uid,epoch):
-
-#     get_epoch = time.mktime(time.strptime(epoch, '%d-%m-%Y %H:%M:%S')) * 1000
-
-#     appoint = db.collection(u'appointment').where(u'user_id', u'==', uid).stream()
-
-#     doc_t = []
-
-
-#     for doc in appoint:
-#             # print(f'{doc.id} => {doc.to_dict()}')
-#             appointment = doc.to_dict()
-#             doc_t.append(appointment)
-
-#     for e in doc_t:
-
-#         if(e['epoch']==get_epoch):
-
-#             epoch_app = e
-#             return jsonify(epoch_app)
-
-#     return f'{get_epoch}'
-
-
-
-@app.route('/dash', methods=['GET','POST'])
-def dashboard():
-    if is_logged_in == True:
-         return redirect(url_for('user'))
-    else:
-        return redirect(url_for('signin'))
 
 @app.route('/approvedDoctor', methods=['GET'])
 def approved():
@@ -364,7 +301,6 @@ def approved():
     doc = []
 
     for docs in query:
-        # print(f'{doc.id} => {doc.to_dict()}')
         appointment = docs.to_dict()
         doc.append(appointment)
 
@@ -372,12 +308,9 @@ def approved():
 
 @app.route('/appointDetails/user=<uid>/epoch=<epoch>', methods=['GET', 'POST'])
 def checkepoch(uid, epoch):
-    # print(epoch)
-    # get_epoch = time.mktime(time.strptime(epoch, '%d-%m-%Y %H:%M:%S')) * 1000
-    # get_epoch1="20-02-2021 15:00:00"
+   
     get_epoch = time.mktime(time.strptime(epoch, '%d-%m-%Y %H:%M:%S')) * 1000
 
-    # print(int(get_epoch))
 
     appoint = db.collection(u'appointment').where(u'doctor_id', u'==', uid).where(u'epoch', u'==',
                                                                                   int(get_epoch)).stream()
@@ -385,7 +318,6 @@ def checkepoch(uid, epoch):
     doc_t = []
 
     for doc in appoint:
-        # print(f'{doc.id} => {doc.to_dict()}')
         appointment = doc.to_dict()
         doc_id = doc.id
 
@@ -394,12 +326,9 @@ def checkepoch(uid, epoch):
 
 @app.route('/uappointDetails/user=<uid>/epoch=<epoch>', methods=['GET', 'POST'])
 def uappoint(uid, epoch):
-    # print(epoch)
-    # get_epoch = time.mktime(time.strptime(epoch, '%d-%m-%Y %H:%M:%S')) * 1000
-    # get_epoch1="20-02-2021 15:00:00"
+    
     get_epoch = time.mktime(time.strptime(epoch, '%d-%m-%Y %H:%M:%S')) * 1000
 
-    # print(int(get_epoch))
 
     appoint = db.collection(u'appointment').where(u'user_id', u'==', uid).where(u'epoch', u'==',
                                                                                 int(get_epoch)).stream()
@@ -407,7 +336,6 @@ def uappoint(uid, epoch):
     doc_t = []
 
     for doc in appoint:
-        # print(f'{doc.id} => {doc.to_dict()}')
         appointment = doc.to_dict()
         doc_id = doc.id
 
@@ -415,12 +343,10 @@ def uappoint(uid, epoch):
 
 @app.route('/appointments/uid=<uid>/epoch=<epoch>',methods=['GET','POST'])
 def view_app(uid,epoch):
-    # print(epoch)
     get_epoch = time.mktime(time.strptime(epoch, '%d-%m-%Y %H:%M:%S')) * 1000
     appoint = db.collection(u'appointment').where(u'user_id', u'==', uid).stream()
     doc_t = []
     for doc in appoint:
-            # print(f'{doc.id} => {doc.to_dict()}')
             appointment = doc.to_dict()
             doc_t.append(appointment)
 
@@ -429,8 +355,7 @@ def view_app(uid,epoch):
             if(e['epoch']== get_epoch):
 
                 epoch_app = e
-                # print(epoch_app)
-                #return "hello"
+                
                 return render_template('base.html',data=e)
             
 
@@ -483,23 +408,7 @@ def dotor():
             for doc in appoint_pa:
                 appointment = doc.to_dict()
                 doc_p.append(appointment)
-        # ress = [sub['test_status'] for sub in total]
-        # for r in ress:
-        #     if r == '1':
-        #         for doc1 in total:
-        #             doc_u.append(doc1)
-        #     else:
-        #         for doc2 in total:
-        #             doc_p.append(doc2)
-
-        # for doc in appoint:
-        #
-        #     # if doc['test_status'] == '0':
-        #     appointment = doc.to_dict()
-        #     doc_u.append(appointment)
-        #     # else:
-        #     #     appointment = doc.to_dict()
-        #     #     doc_p.append(appointment)
+        
 
             res_u = [sub['epoch'] for sub in doc_u]
             res_p = [sub['epoch'] for sub in doc_p]
@@ -516,7 +425,6 @@ def dotor():
                 return render_template("doctorDetails.html",user=userList,uid=doc_id)
 
             else:
-            # print("total",total)
 
                 return render_template("doctorDetails.html", tot=total, up=res_up, past=res_pa, time=res2, uid=doc_id,
                                    user=userList)
@@ -578,12 +486,10 @@ def orderDet():
         if doc:
 
             if "@" in email:
-                # order_l = [sub['order_id'] for sub in doc_t]
-                # print(doc_t)
+               
                 return render_template("orderDetails.html",orderList=doc_t)
 
             else:
-                # print(doc_o)
                 return render_template("orderDetails.html",orderDet=userList)
         
             
@@ -592,13 +498,11 @@ def orderDet():
 @app.route('/editDoctor/uid=<uid>', methods=['GET', 'POST'])
 def editDoctor(uid):
     try:
-        # if request.method == 'POST':
 
         addr = request.form['addr']
         name = request.form['uname']
         db.collection(u'doctor').document(uid).update({'name': name, 'address_google_map': addr})
 
-        # return render_template("userDetails.html", us=res2, uid=doc_id, user=userList)
         return render_template('doctor.html')
 
     except Exception as e:
@@ -608,7 +512,6 @@ def editDoctor(uid):
 @app.route('/dAppoint/uid=<uid>', methods=['GET', 'POST'])
 def dappoint(uid):
     try:
-        # if request.method == 'POST':
         pname = request.form['pname']
         dname = request.form['dname']
         db.collection(u'appointment').document(uid).update({'user_name': pname, 'dotor_name': dname})
@@ -621,7 +524,6 @@ def dappoint(uid):
 @app.route('/uAppoint/uid=<uid>', methods=['GET', 'POST'])
 def appoint(uid):
     try:
-        # if request.method == 'POST':
         pname = request.form['pname']
         dname = request.form['dname']
         db.collection(u'appointment').document(uid).update({'user_name': pname, 'dotor_name': dname})
@@ -638,81 +540,15 @@ def pending():
     doc = []
 
     for docs in query:
-        # print(f'{doc.id} => {doc.to_dict()}')
         appointment = docs.to_dict()
         doc.append(appointment)
 
     return render_template('pendingAppoint.html', data=doc)
 
 
-@app.route('/signin', methods=['GET', 'POST'])
-def signin():
-
-    global is_logged_in
-
-    session.permanent = True
-
-    is_logged_in = False
-
-    uns = "Please verify your mail"
-    inv = "Invalid email or incorrect password"
-
-    if request.method == 'POST':
-        email = request.form['name']
-        password = request.form['pass']
-
-        try:
-
-            user = auth.sign_in_with_email_and_password(email, password)
-            acc = auth.get_account_info(user["idToken"])
-
-            temp = json.dumps(acc)
-    
-            acc_info = json.loads(temp)
-
-            user_info = acc_info["users"]
-
-            emailVerified = str([sub['emailVerified'] for sub in user_info])
 
 
-            if(emailVerified.lower() != "[false]"):
-                
-                is_logged_in = True
 
-                doc_ref = db.collection('admin').document(user["localId"])
-                doc = doc_ref.get()
-                if doc.exists:
-                    db.collection("admin").document(user["localId"]).update({u'verified': True})
-                
-                else:
-                    data = { 'email': email , 'verified': True}
-                    db.collection("admin").document(user["localId"]).set(data)
-            
-                return redirect(url_for('dashboard'))
-	            
-    
-            else:
-                return  render_template('signinpage.html', us=uns)
-
-                
-        except:
-             return render_template('signinpage.html',msg=inv)
-    else:
-        if is_logged_in == True:
-            return redirect(url_for('user'))
-        else:
-            return render_template('signinpage.html')
-
-@app.route('/list', methods=['GET'])
-def read():
-   
-    try:
-        
-        all_todos = [doc.to_dict() for doc in todo_ref.stream()]
-        
-        return render_template('page.html',t= all_todos)
-    except Exception as e:
-        return f"An Error Occured: {e}"
 
 # @app.before_request
 # def make_session_permanent():
